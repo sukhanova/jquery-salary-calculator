672033cc4fbd02e74f75fa9636b4a9b7160abb6e
sukhanova <verusia.s@gmail.com>
2020-07-24 21:58:53 -0500

    added html to the project

---
+++ client.js
@@ -0,0 +1,16 @@
+/* 
+JavaScript:
+- link jQuery
+- create variable to hold all employees array
+- create variable to hold each employee monthly salary (annual / 12)
+- create variable to hold monthly total
+- create employee object(firstName, lastName, ID, title, annualSalary)
+- push employee to an array (function)
+
+Display on DOM
+- add to table (function)
+- update total (function)
+- delete employee/row (function)
+- update total
+- if total monthly cost > 20000 - add red background color to the total monthly cost
+*/

1979407cab93c56f3eda89546cf22d128d945f10
sukhanova <verusia.s@gmail.com>
2020-07-25 18:37:00 -0500

    added solution for rounding total to 2 decimals

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 $(document).ready(readyNow);
 
 let employeesList = [];
+let monthlyTotal = 0;
 
 function readyNow(){
     console.log('jquery is running');
@@ -17,7 +18,7 @@ function addEmployee(){
         lastName: $('#lastNameInput').val(),
         id: $('#idInput').val(),
         title: $('#titleInput').val(),
-        annualSalary: Number($('#annualSalaryInput').val())
+        annualSalary: $('#annualSalaryInput').val()
     }
     // add employee to employeeList array:
 
@@ -54,22 +55,30 @@ function appendEmployeesToDOM(){
         <td><button class="deleteBtn">Delete</button></td>
        <tr>
        `);
-   }
-   
+
+       monthlyTotal += (eachEmployee.annualSalary/12);
+       $('#employeeMonthlyTotal').html(`$${monthlyTotal.toFixed(2)}`);
+        
+       //console.log(typeof monthlyTotal);
+   //console.log(`employees monthly salary total:$ ${monthlyTotal}`);
+  }
+  
 }
 
 function deleteEmployee(){
     //console.log('click in deleteEmployee!');
-    $(this).closest('tr').toggle('##displayEmployeeInfo');
+    $(this).closest('tr').toggle('#displayEmployeeInfo');
 }
+
+
 
 /*
 
 JavaScript:
 - [x] link jQuery
 - [x] create variable to hold all employees array
-- create variable to hold each employee monthly salary (annual / 12)
-- create variable to hold monthly total
+- [x] create variable to hold each employee monthly salary (annual / 12)
+- [x] create variable to hold monthly total
 - [x] create employee object(firstName, lastName, ID, title, annualSalary)
 - [x] push employee to an array (function)
 
